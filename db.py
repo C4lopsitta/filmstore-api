2ec3030f9fc9e9022c15caed4cafdc22c97eddad
c4lopsitta <simone@atomtech.cc>
2024-07-23 20:51:12 +0200

    Fixed initialization in db

--- db.py
+++ db.py
@@ -9,7 +9,48 @@ connection = sqlite3.connect('./filmstore.sqlite')
 
 cursor = connection.cursor()
 
-subprocess.run(['sqlite3', 'filmstore.sqlite', '-init', './init.sql'])
+cursor.execute("""
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE if not exists films(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    iso INTEGER NOT NULL,
+    info TEXT,
+    type INTEGER NOT NULL,
+    format INTEGER
+);
+
+CREATE TABLE if not exists pictures(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT,
+    location TEXT,
+    aperture TEXT,
+    shutter TEXT,
+    posted INTEGER,
+    printed INTEGER,
+    thumbnail TEXT NOT NULL
+);
+
+CREATE TABLE if not exists filmrolls(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    film INTEGER,
+    archival TEXT,
+    status INTEGER NOT NULL,
+    camera TEXT,
+    FOREIGN KEY (film) REFERENCES films(id)
+);
+
+CREATE TABLE if not exists pic_film_rel(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    filmroll INTEGER,
+    picture INTEGER,
+    FOREIGN KEY (filmroll) REFERENCES filmrolls(id),
+    FOREIGN KEY (picture) REFERENCES pictures(id)
+);
+""")
+
+connection.commit()
 
 
 def add_film(film: Film):
